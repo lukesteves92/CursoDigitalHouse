575d6fd8fec74304c2fc15d43972f9af679531c6
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-05-10 19:34:36 -0300

    Aula08

--- src/ChecarNumero.kt
+++ src/ChecarNumero.kt
@@ -1,4 +1,31 @@
-package Exercicio1Banco
+fun main(){
 
-class ChecarNumero {
+    checarNumero(4)
+
+    checarNumeroImpar()
 }
+
+fun checarNumero(numero: Int):Int{
+
+    when {
+        numero%2 == 0 -> println("Esse número é par")
+        else -> println("Esse número é Impar")
+    }
+    return numero
+}
+
+fun checarNumeroImpar(){
+    var numero = 1
+    var cont = 0
+
+    while (cont < 100){
+        if (numero % 2 != 0){
+            println(numero)
+            cont++
+        }
+        numero++
+
+    }
+
+}
+
--- src/TesteGabi.kt
+++ src/TesteGabi.kt
@@ -1,2 +1,7 @@
-class TesteGabi {
+fun main (){
+    var Nota1: Float = 5.0F
+    var Nota2 = 7.1f
+    var soma = Nota1 + Nota2
+    var Media: Float = soma / 2.00f
+    println("Media = $Media")
 }
