ab025745f1c67115fd3a1de52bf24c36d980f985
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-05-13 02:35:43 -0300

    Exercicio Estoque

--- src/ExercicioEstoque.kt
+++ src/ExercicioEstoque.kt
@@ -1,12 +1,6 @@
-class ExercicioEstoque (var nome: String, var qtdAtual: Int, var qtdMinima: Int) {
+class EstoqueProduto (var nome: String, var qtdAtual: Int, var qtdMinima: Int) {
 
 
-
-
-
-
-
-    
 
     fun mudarNome(nome: String){
         this.nome = nome
@@ -14,23 +8,32 @@ class ExercicioEstoque (var nome: String, var qtdAtual: Int, var qtdMinima: Int)
 
     fun mudarQtdMinima(qtdMinima: Int){
 
+        this.qtdMinima = qtdMinima
 
     }
 
     fun repor(qtd: Int){
 
+        qtdAtual += qtd
 
     }
 
     fun darBaixa(qtd: Int){
+
+        qtdAtual -= qtd
     }
 
-    fun precisaRepor(){
+    fun precisaRepor(): Boolean{
 
+        return qtdAtual <= qtdMinima
 
     }
 
    fun mostra(): String{
+
+       return "Nome do produto: $nome, +" +
+               "Quantidade Atual: $qtdAtual+" +
+               "Quantidade minima: $qtdMinima"
    }
 
 
