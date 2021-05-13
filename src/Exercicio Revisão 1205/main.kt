3205250c6285d1fb050e5932c65cc956ac5e92f6
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-05-13 02:03:09 -0300

    Exercicio Estoque

--- src/Exercicio Revisão 1205/main.kt
+++ src/Exercicio Revisão 1205/main.kt
@@ -18,4 +18,3 @@ fun main(){
                 "acadêmico é ${it.registroAcademico}")
     }
 }
-
---
+++ src/ExercicioEstoque.kt
@@ -0,0 +1,48 @@
+class ExercicioEstoque (var nome: String, var qtdAtual: Int, var qtdMinima: Int) {
+
+
+
+
+
+
+
+    
+
+    fun mudarNome(nome: String){
+        this.nome = nome
+    }
+
+    fun mudarQtdMinima(qtdMinima: Int){
+
+
+    }
+
+    fun repor(qtd: Int){
+
+
+    }
+
+    fun darBaixa(qtd: Int){
+    }
+
+    fun precisaRepor(){
+
+
+    }
+
+   fun mostra(): String{
+   }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
