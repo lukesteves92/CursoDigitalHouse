4217ca374abfc29f28eef400fc243daf293e1901
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-05-20 03:02:42 -0300

    ExercicioEscola

--- src/ExercicioRevisão1205/Exercicio.kt
+++ src/ExercicioRevisão1205/Exercicio.kt
@@ -1,33 +1,57 @@
 package ExercicioRevisão1205
 
+import java.util.*
+import javax.swing.JOptionPane
+
 
 fun main() {
 
-    val professor = Professor("Lucas", "Coelho", "12345")
+    val professor = Professor("Joaquim", "Neves", "505050")
+    val professor2 = Professor("Leandro", "Silva", "505050")
     val materiaKotlin = Materia("Android")
     val aulaKotlin = Aula(
         materiaKotlin, "19:30", "21:45"
     )
     val Lucas = Aluno("123456", "Lucas", "Esteves")
-    val Jaqueline = Aluno("12345", "Jaqueline", "Melo")
-    val curso = Curso("Curso Android", listOf(aulaKotlin), listOf(Lucas, Jaqueline), professor)
+    val Pedro = Aluno("12345", "Pedro", "Melo")
+    val curso = Curso("Curso Android", listOf(aulaKotlin), listOf(Lucas, Pedro), listOf(professor, professor2))
     val turmaTop = Turma("Turma 1 - Android", curso)
 
+var nomeUsuario = "0"
 
-    turmaTop.curso.listaAlunos.forEach {
-        println(
-            "O nome do aluno é ${it.nome} ${it.sobrenome} e o registro " +
-                    "acadêmico é ${it.registroAcademico} e +" +
-                    "${it.assistirAula()}"
+var opcao = 0
+    while (opcao < 1 || opcao > 6) {
+         opcao = Integer.parseInt(
+            JOptionPane.showInputDialog(
+                "Menu Digital House:+" +
+                        "1 - Consultar Alunos+" +
+
+                        "2 - Consultar Professores+" +
+
+                        "3 - Consultar Cursos disponiveis" +
+
+                        "4 - Consultar RegistroAcademico" +
+
+                        "5 - Sair"
+            )
         )
     }
+
+    when (opcao) {
+        1 -> curso.listaAlunos.forEach { JOptionPane.showMessageDialog(null, it.nome) }
+        2 -> curso.listaProfessor.forEach { JOptionPane.showMessageDialog(null, it.nome) }
+        3 -> JOptionPane.showMessageDialog(null, curso.nome)
+        4 -> curso.listaAlunos.forEach{JOptionPane.showMessageDialog(null, it.registroAcademico)}
+        5 -> JOptionPane.showMessageDialog(null, "Volte Sempre!")
+
+    }
+
 }
 
 class Aluno(var registroAcademico: String, nome: String, sobrenome: String) : Pessoa(nome, sobrenome) {
 
     fun assistirAula() {
 
-        println("está assitindo aula")
     }
     fun fazerLicao() {
     }
@@ -39,7 +63,7 @@ class Curso(
     var nome: String,
     var listaAulas: List<Aula>,
     var listaAlunos: List<Aluno>,
-    var professor: Professor
+    var listaProfessor: List<Professor>
     ) {
 }
 
