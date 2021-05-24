e8f4f4cd0c846711b9e274b16c67de85262f3628
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-05-24 19:46:09 -0300

    Aula-14

---
+++ src/Aula14.kt
@@ -0,0 +1,25 @@
+
+fun main(){
+
+    nomeEIdadeDefault()
+
+}
+
+
+fun nomeEIdade(nome: String, Idade: String){
+
+
+}
+
+fun nomeEIdadeComRetorno(nome: String, idade: String): String{
+
+return  "O nome é $nome e a idade é $idade"
+
+
+}
+
+fun nomeEIdadeDefault(nome: String = "Fulano", sobrenome: String = "De Tal"){ //Default Arguments
+
+
+
+}
