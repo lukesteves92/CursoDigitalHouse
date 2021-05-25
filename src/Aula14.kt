80dfbc86f8f7299484efd6bc0f0c9e5ca25b7e14
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-05-24 21:19:17 -0300

    Aula-14

--- src/Aula14.kt
+++ src/Aula14.kt
@@ -3,6 +3,25 @@ fun main(){
 
     nomeEIdadeDefault()
 
+    testeLambda {
+        return@testeLambda "Teste"
+
+    }
+
+    val animais = listOf("Peixe", "Cachorro")
+
+    animais.forEach{
+        println(it)
+    }
+
+    val b: String? = null
+    println(b?.length ?: 0) // Elvis operator
+
+    b?.length ?: run{
+        val soma = 2 + 2
+        println(soma)
+    }
+
 }
 
 
@@ -20,6 +39,18 @@ return  "O nome é $nome e a idade é $idade"
 
 fun nomeEIdadeDefault(nome: String = "Fulano", sobrenome: String = "De Tal"){ //Default Arguments
 
+
+
+}
+
+// Função Lambdas -> Função Anonima
+
+fun testeLambda(xpto: () -> String){ // Passando função como parametro
+    println("A String da minha função anonima é : ${xpto()}")
+
+}
+
+class NullSafety(val nome: String? = "Cesar", val sobrenome: String){
 
 
 }
