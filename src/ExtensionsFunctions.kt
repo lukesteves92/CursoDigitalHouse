0c042763b0d9ce86cb66f94c5d519369d86bc0ee
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-05-26 20:24:48 -0300

    Aula-15

--- src/ExtensionsFunctions.kt
+++ src/ExtensionsFunctions.kt
@@ -1,12 +1,30 @@
-fun main(){
+import java.text.NumberFormat
+import java.util.*
 
-    val nome: String = "Lucas Esteves"
-    nome.colocarSobrenome(lastName = "Coelho")
+fun main() {
+    val nome = "Cesar Rodrigues"
+    println(nome.colocarSobrenome(lastName = "Nascimento"))
+
+    val cesarItau = ContaCorrente(1234, taxaDeOperacao = 2.0)
+    cesarItau.depositar(102.0)
+    cesarItau.printarOSaldoAtual()
+
+    println("Digite o valor em reais:")
+    val scanner = Scanner(System.`in`)
+    val real = readLine()?.toDouble()
+    println(real?.formatarParaReal())
 }
 
+fun String.colocarSobrenome(lastName: String) = "$this $lastName"
 
-fun String.colocarSobrenome(lastName: String): String{
+fun ContaCorrente.printarOSaldoAtual() = println("O meu saldo é ${this.saldo}")
 
-    return "$this $lastName"
+fun Double.formatarParaReal(): String {
+    val numberFormat = NumberFormat
+        .getCurrencyInstance(Locale("pt", "BR"))
+    return numberFormat.format(this)
+}
 
+fun Int.formatarInteiro() {
+    println(this)
 }
