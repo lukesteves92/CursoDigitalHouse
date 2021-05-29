baf03878a7b276a7ff3a059d318e79244a72fef5
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-05-28 23:24:00 -0300

    Aula-16

---
+++ src/TesteCollections.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    val listaInteiros = listOf(1, 2, 3, 4, 5)
+    var somatotal = 0
+
+    for (i in listaInteiros){
+
+        somatotal += i
+
+    }
+
+    println(somatotal)
+
+    
+}
