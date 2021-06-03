5fb9de2e7fb485689019dfe9951fe9fd07b72f56
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-06-02 22:38:24 -0300

    UpdateDesafio

--- src/DesafioKotlinDH2021/DigitalHouseManager.kt
+++ src/DesafioKotlinDH2021/DigitalHouseManager.kt
@@ -10,5 +10,33 @@ class DigitalHouseManager {
     private val matriculas: MutableList<Matricula> = mutableListOf()
 
 
+    fun registrarCurso(nome: String, codigo: Int, qtdMaxAlunos: Int, professorT: ProfessorTitular, professorA: ProfessorAdjunto) {
+         for (i in cursos){
+            if (i.codigoCurso != codigo) {
+                val curso = Curso(nome, codigo, qtdMaxAlunos, professorT, professorA)
+                println("Curso registrado com sucesso.")
+
+            }else{
+                println("ERRO! Já existe um curso registrado com o código '$codigo'.")
+
+            }
+        }
+    }
+
+    fun excluirCurso(codigo: Int) {
+        for (i in cursos){
+            if (i.codigoCurso == codigo) {
+                cursos.remove(i)
+                println("Curso removido com sucesso")
+
+            }else{
+                println("ERRO! Não existe um curso com este código '$codigo'.")
+
+            }
+        }
+    }
+
 
 }
+
+
